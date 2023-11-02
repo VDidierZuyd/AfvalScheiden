6eb27756a2229508a19ddf5fe7fe349631dd5724
DidierV <2205137vervier@zuyd.nl>
2023-11-02 10:46:03 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AfvalScheiden.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
